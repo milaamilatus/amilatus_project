beebee8f179a544be2c6b00ffc9e1fb7fa60bca3
milaamilatus <148957332+milaamilatus@users.noreply.github.com>
2023-10-25 20:05:25 +0700

    Add files via upload

---
+++ project_saya/project_saya/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for project_saya project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_saya.settings')
+
+application = get_asgi_application()
